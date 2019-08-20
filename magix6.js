207e9ee15691424704a2765989f3f2e4e2ac7799
pelletsstarPL <54317957+pelletsstarPL@users.noreply.github.com>
2019-08-20 20:19:52 +0200

    Update magix6.js

--- magix6.js
+++ magix6.js
@@ -5,6 +5,6 @@ desc:'Magic! Magic!. Fit more guys discover essences which have its secret use.'
 engineVersion:1,
 manifest:0,
 requires:['Default dataset*'],
-sheets:{'magixmod':'https://imgur.com/a/91Ln5DX'},//custom stylesheet (note : broken in IE and Edge for the time being)
+sheets:{'magixmod.png':'https://github.com/pelletsstarPL/Magixmod/blob/master/magixmod.png'},//custom stylesheet (note : broken in IE and Edge for the time being)
 func:function()
 })
