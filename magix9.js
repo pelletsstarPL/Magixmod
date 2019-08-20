0663157b1110d5c844354218e23a85c19a59fdc0
pelletsstarPL <54317957+pelletsstarPL@users.noreply.github.com>
2019-08-20 20:24:00 +0200

    Update and rename magix8.js to magix9.js

--- magix9.js
+++ magix9.js
@@ -5,6 +5,8 @@ desc:'Magic! Magic!. Fit more guys discover essences which have its secret use.'
 engineVersion:1,
 manifest:0,
 requires:['Default dataset*'],
-sheets:{'magixmod.png':'https://raw.githubusercontent.com/pelletsstarPL/Magixmod/master/magixmod.png'},//custom stylesheet (note : broken in IE and Edge for the time being)
+sheets:{
+        'magixmod.png':'https://raw.githubusercontent.com/pelletsstarPL/Magixmod/master/magixmod.png'
+},//custom stylesheet (note : broken in IE and Edge for the time being)
 func:function()
 })
