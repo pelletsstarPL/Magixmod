8ff375f057967dac163981fdf2a7b94e37fd77b4
pelletsstarPL <54317957+pelletsstarPL@users.noreply.github.com>
2019-08-27 15:41:59 +0200

    Update magix80.js

--- magix80.js
+++ magix80.js
@@ -392,6 +392,7 @@ func:function(){
     			{type:'provide',what:{'Land of the Plain Island':25000}},//SOON NEW CATEGORY & BUILDINGS WHICH CAN ONLY BE BUILT THERE
     		],
     		use:{'land':10},
+		messageOnStart:'You built a portal to Plain Island. It is big isle. On this island you may build houses , mines and other but not these one you built in your mortal world. You will unlock new category of buildings, a little bit better but limited housing. You may gain new minerals, who know maybe new food or anything else you did not see anytime earlier.',
     		req:{'First portal to new world':true,'Belief in portals':true},
     		limitPer:{'land':100000000000000},//It is something like max 1
     		category:'dimensions',
