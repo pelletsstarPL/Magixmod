946879b2f9e05dc02bc7984886180e0285c91b1c
pelletsstarPL <54317957+pelletsstarPL@users.noreply.github.com>
2019-08-28 11:28:00 +0200

    Update magix80.js

--- magix80.js
+++ magix80.js
@@ -423,7 +423,6 @@ func:function(){
 		cost:{'basic building materials':100},
 		use:{'Land of the Plain Island':5,'worker':4,'metal tools':4},
 		modes:{
-			'off':G.MODE_OFF,
 			'cobalt':{name:'Cobalt',icon:[8,2,'magixmod'],desc:'Gain [Cobalt ore]. Will consume more due to hardness of Cobalt.',use:{'worker':5,'stone tools':3,'metal tools':4}},
 			'copper':{name:'Copper',icon:[9,8],desc:'Mine for [copper ore] mainly.',req:{'prospecting':true},use:{'worker':4,'metal tools':4}},
 			'salt':{name:'Salt',icon:[11,7],desc:'Mine for [salt] mainly.',req:{'prospecting':true},use:{'worker':3,'metal tools':3}},
