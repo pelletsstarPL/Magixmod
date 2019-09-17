1c9263cd080b81e2116e7f81ad7548cbd57cefc1
pelletsstarPL <54317957+pelletsstarPL@users.noreply.github.com>
2019-09-17 17:35:44 +0200

    Update magix136.js

--- magix136.js
+++ magix136.js
@@ -1586,6 +1586,7 @@ func:function(){
 			name:'Weave leather colored clothing',
 			icon:[13,0,'magixmod'],
 			desc:'Your clothier will now weave [leather] but colored clothing.',
+			req:{'weaving':true},
 			use:{'worker':1,'stone tools':1},
 		};	
 		G.getDict('clothier').effects.push({type:'convert',from:{'leather':2,'Dyes':3},into:{'Colored clothing':1},every:6,mode:'Weave leather colored clothing'});
@@ -1593,6 +1594,7 @@ func:function(){
 			name:'Weave fiber colored clothing',
 			icon:[13,0,'magixmod'],
 			desc:'Your clothier will now weave fiber but colored clothing.',
+			req:{'weaving':true},
 			use:{'worker':1,'stone tools':1},
 		};
 		G.getDict('clothier').effects.push({type:'convert',from:{'herb':52,'Dyes':4},into:{'Colored clothing':1},every:6,mode:'Weave fiber colored clothing'});
@@ -1600,6 +1602,7 @@ func:function(){
 			name:'Weave fiber colored clothing',
 			icon:[13,0,'magixmod'],
 			desc:'Your clothier will now dye already made [basic clothes] making them become[Colored clothing].',
+			req:{'weaving':true},
 			use:{'worker':1,'stone tools':1},
 		};
 		G.getDict('clothier').effects.push({type:'convert',from:{'basic clothes':1,'Dyes':4},into:{'Colored clothing':1},every:6,mode:'Dye already made clothing'});
