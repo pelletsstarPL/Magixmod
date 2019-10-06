5ed557aa2013ff94cae6d11b37bcf733e4ea3f3c
pelletsstarPL <54317957+pelletsstarPL@users.noreply.github.com>
2019-10-06 10:34:11 +0200

    Update magix172.js

--- magix172.js
+++ magix172.js
@@ -1700,7 +1700,7 @@ func:function(){
 		new G.Tech({
 		name:'Medicaments brewing',
 		desc:'[Alchemists] will now be able to craft at their stands medicaments out of [Flowers,various flowers] and a [herb,herbs].',
-		icon:[18,2,'magixmod'],
+		icon:[19,6,'magixmod'],
 		cost:{'insight':750},
 		req:{'Beginnings of alchemy':true},
 	});
