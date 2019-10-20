3bb2cc3441a7a0454cedb3ab14cd58639b1111a1
pelletsstarPL <54317957+pelletsstarPL@users.noreply.github.com>
2019-10-20 15:10:47 +0200

    Update and rename magix173.js to magix173test0.js

--- magix173test0.js
+++ magix173test0.js
@@ -3430,7 +3430,7 @@ G.writeMSettingButton=function(obj)
 		'<div class="fancyText title">Will you add more wonders or portals?</div>'+
 		'Yes i will... unless i run out of ideas so then i won\'t :D'+
 		'<div class="fancyText title">How i can ask you by question which is not in Q&A there?</div>'+
-		'Find me at <a href="https://discordapp.com/invite/cookie" target="_blank">Dashnet discord server</a>.'+
+		'Find me at <a href="https://discordapp.com/invite/cookie" target="_blank">Dashnet discord server</a><div>.'+
 		G.dialogue.getCloseButton()+
 		'</div>';
 		return str;
