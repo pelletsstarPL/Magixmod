b21f1b9cf5425b4727c7c37e385310a72b8d82fd
pelletsstarPL <54317957+pelletsstarPL@users.noreply.github.com>
2019-10-22 05:33:24 +0200

    Update and rename magix173testificate.js to magix173testificates.js

--- magix173testificates.js
+++ magix173testificates.js
@@ -3477,6 +3477,7 @@ G.writeMSettingButton=function(obj)
 		'<div class="fancyText title">How i can ask you by question which is not in Q&A there?</div>'+
 		'Find me at <a href="https://discordapp.com/invite/cookie" target="_blank">Dashnet discord server</a><div>'+
 		'<div class="barred fancyText">Update log'+
+		'added example heellooo world!'+
 		G.writeMSettingCategories()+
 		'<div class="divider"></div>'+
 		'<div class="buttonBox">'+
