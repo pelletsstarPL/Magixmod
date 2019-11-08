0a6d0c3e39960991bd6c61290c42708d9ed0fbc0
pelletsstarPL <54317957+pelletsstarPL@users.noreply.github.com>
2019-11-08 18:10:50 +0100

    Update and rename magix175pre1.js to magix175pre2.js

--- magix175pre2.js
+++ magix175pre2.js
@@ -56,6 +56,24 @@ func:function(){
 				side:[],
 		},
 	};
+	var limitDesc=function(limit){return 'It is limited by your '+limit+'; the closer to the limit, the slower it is to produce more.';};
+	var researchGetDisplayAmount=function()
+		{
+			var limit=G.getDict(this.limit).displayedAmount;
+			return B(this.displayedAmount)+'<wbr>/'+B(limit);
+		};
+	var researchWhenGathered=function(me,amount,by)
+		{
+			var limit=G.getDict(this.limit).amount;
+			if (limit>0)
+			{
+				var mult=1;
+				if (G.year<5) mult=1.25;//faster research the first 5 years
+				me.amount+=randomFloor(Math.pow(1-me.amount/limit,2)*(Math.random()*amount*me.mult*mult));
+				me.amount=Math.min(me.amount,limit);
+			}
+		};
+	
 		new G.Res({
 		name:'Mana',
 		desc:'[Mana] is used to make essences. Thing used by beginner wizards. ',
