b84fcd4c7fda3f33cbd250d6e806369352a37bc9
pelletsstarPL <54317957+pelletsstarPL@users.noreply.github.com>
2019-11-09 09:53:52 +0100

    Update and rename magix175pre23.js to magix175pre24.js

--- magix175pre24.js
+++ magix175pre24.js
@@ -3606,7 +3606,7 @@ func:function(){
 		{type:'function',func:function(me){
 		var wiggleRoom=15;
 		var toMake=Math.min(me.amount-me.idle,Math.max(0,(G.getRes('population').amount+wiggleRoom)-(G.getRes('housing').amount-G.getRes('burial spot').used)));
-		if (toMake>0 && G.canBuyUnitByName('Brick house with a silo',toMake))
+		if (toMake>0 && G.canBuyUnitByName('house',toMake))
 		{
 			G.buyUnitByName('Brick house with a silo',toMake,true);
 		}
@@ -3623,7 +3623,7 @@ func:function(){
 		{type:'function',func:function(me){
 		var wiggleRoom=50;
 		var toMake=Math.min(me.amount-me.idle,Math.max(0,(G.getRes('population').amount+wiggleRoom)-(G.getRes('housing').amount-G.getRes('burial spot').used)));
-		if (toMake>0 && G.canBuyUnitByName('Blockhouse',toMake))
+		if (toMake>0 && G.canBuyUnitByName('house',toMake))
 		{
 			G.buyUnitByName('Blockhouse',toMake,true);
 		}
