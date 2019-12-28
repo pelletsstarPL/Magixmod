f2f31fe24d6f811431547d74032046ed8f1a38f1
pelletsstarPL <54317957+pelletsstarPL@users.noreply.github.com>
2019-12-28 20:34:21 +0100

    Update and rename MagixOfficial_b178tr9.js to MagixOfficial_b178tr10.js

--- MagixOfficial_b178tr10.js
+++ MagixOfficial_b178tr10.js
@@ -1006,7 +1006,6 @@ func:function(){
 		{
 			if (me.amount=1.01){ 
 				G.Message({type:'good',text:'<b>You and your people activated passage to Paradise. Out of portal an Emblem fall and hits next to your feet. Big golden portal shines and you can see what is beyond it. You come through and notice there is perfect heat to live. Now it is time for more discoveries.</b>',icon:[8,4,'magixmod']});
-				var n=0.01;G.lose('Paradise emblem',n,'debug');
 			}
 		},	
 		category:'main',
