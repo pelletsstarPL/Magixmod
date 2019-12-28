ec370f9fe008fb99bb0850a49a521faa48cd5c83
pelletsstarPL <54317957+pelletsstarPL@users.noreply.github.com>
2019-12-28 20:23:50 +0100

    Update and rename MagixOfficial_b178tr7.js to MagixOfficial_b178tr8.js

--- MagixOfficial_b178tr8.js
+++ MagixOfficial_b178tr8.js
@@ -1001,11 +1001,12 @@ func:function(){
 		name:'Paradise emblem',
 		desc:'A thing you will get after activating a Paradise portal. Needed to unlock further researching. A pass for further things and more adventures. You can obtain only one Emblem of this type. <b>@God called you to his world... But God\'s paradise has rules that you & your people must follow or Paradise will be closed for you and your people... so in fact your adventure has not ended yet even after adventure in Plain Island. Good luck.<b>',
 		icon:[20,9,'magixmod'],
-		startWith:0,
 		tick:function(me,tick)
 		{
 			if (me.amount=1.01){ 
 				G.Message({type:'good',text:'<b>You and your people activated passage to Paradise. Out of portal an Emblem fall and hits next to your feet. Big golden portal shines and you can see what is beyond it. You come through and notice there is perfect heat to live. Now it is time for more discoveries.</b>',icon:[8,4,'magixmod']});
+			}
+			if (me.amount=1.01){
 				var toSpoil=me.amount*0.009900990099009901;
 				var spent=G.lose(me.name,randomFloor(toSpoil),'debug2message');
 			}
