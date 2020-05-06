9f809cb8d2a033568dacec56457b00558ca35bce
pelletsstarPL <54317957+pelletsstarPL@users.noreply.github.com>
2020-05-06 10:52:29 +0200

    Update and rename civ2test25.js to civ2test26.js

--- civ2test26.js
+++ civ2test26.js
@@ -1503,10 +1503,9 @@ if (!document.getElementById(cssId))
 		name:'battery of discoveries',
 		desc:'Battery that can be charged by [dreamer]s while in specific mode. A fully charged battery allows for 1 tech rolling or 1 tech reroll.',
 		icon:[4,0,'c2'],
-		displayUsed:true,
-		getDisplayAmount:function(me)
+		getDisplayAmount:function()
 		{
-			var amount=me.amount;
+			var amount=G.getRes('battery of discoveries').amount;
 			if (amount>100) amount=100;
 			return B(amount)+'%';
 		},
