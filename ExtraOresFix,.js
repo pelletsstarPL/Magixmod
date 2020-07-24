29261ab6849f06948aefc023eb51b541cf5e6447
pelletsstarPL <54317957+pelletsstarPL@users.noreply.github.com>
2020-07-24 11:33:20 +0200

    Update and rename ExtraOresFix.js to ExtraOresFix,.js

--- ExtraOresFix,.js
+++ ExtraOresFix,.js
@@ -6,6 +6,8 @@ engineVersion:1,
 manifest:'ModManifest.js',
 requires:['Default dataset*'],
 func:function(){
+  if(G.year>=2){
 G.contextNames['deep mining']='Deep mining';
 G.contextNames['deep quarrying']='Deep quarrying';
+  }
 }});
