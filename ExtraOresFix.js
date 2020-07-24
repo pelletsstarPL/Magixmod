b0be09b0a4281670c177756fcf28a69afe0e53af
pelletsstarPL <54317957+pelletsstarPL@users.noreply.github.com>
2020-07-24 11:25:43 +0200

    Create ExtraOresFix.js

---
+++ ExtraOresFix.js
@@ -0,0 +1,11 @@
+G.AddData({
+name:'Extra ores contexts',
+author:'pelletsstarPL',
+desc:'fix',
+engineVersion:1,
+manifest:'ModManifest.js',
+requires:['Default dataset*'],
+func:function(){
+G.contextNames['deep mining']='Deep mining';
+G.contextNames['deep quarrying']='Deep quarrying';
+}});
