e20a0f62941783a22e5d83ab27343f9ac611cdc9
pelletsstarPL <54317957+pelletsstarPL@users.noreply.github.com>
2020-12-06 09:52:06 +0100

    Update and rename Dungeons2loader_b2.js to Dungeons2loader_b3.js

--- Dungeons2loader_b3.js
+++ Dungeons2loader_b3.js
@@ -136,7 +136,7 @@ a.control.middle{left:48px;top:48px;background-position:-48px -48px;}
 `
     document.body.appendChild(style)
     Game.LoadMod("http://orteil.dashnet.org/cookieclicker/betadungeons/DungeonGen.js")
-    Game.LoadMod("https://cdn.jsdelivr.net/gh/pelletsstarPL/Magixmod@master/dungeons2_b2.js")
+    Game.LoadMod("https://cdn.jsdelivr.net/gh/pelletsstarPL/Magixmod@master/dungeons2_b3.js")
     const intervalId = setInterval(() => {
         if (!window.LaunchDungeons) return
         clearInterval(intervalId)
