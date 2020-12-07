0b25ec1498240009d8abd5af14927476dbbcc1af
pelletsstarPL <54317957+pelletsstarPL@users.noreply.github.com>
2020-12-07 14:42:20 +0100

    Update and rename Dungeons2loader_b47c.js to Dungeons2loader_b47d.js

--- Dungeons2loader_b47d.js
+++ Dungeons2loader_b47d.js
@@ -136,7 +136,7 @@ a.control.middle{left:48px;top:48px;background-position:-48px -48px;}
 `
     document.body.appendChild(style)
     Game.LoadMod("https://cdn.jsdelivr.net/gh/pelletsstarPL/Magixmod@master/dungeon2Gen_b2.js")
-    Game.LoadMod("https://cdn.jsdelivr.net/gh/pelletsstarPL/Magixmod@master/dungeons2_b46.js")
+    Game.LoadMod("https://cdn.jsdelivr.net/gh/pelletsstarPL/Magixmod@master/dungeons2_b47.js")
     const intervalId = setInterval(() => {
         if (!window.LaunchDungeons) return
         clearInterval(intervalId)
