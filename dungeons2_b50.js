228341468cf7610ea09e68d6ff7c86426ea5c405
pelletsstarPL <54317957+pelletsstarPL@users.noreply.github.com>
2020-12-09 10:29:28 +0100

    Update dungeons2_b50.js

--- dungeons2_b50.js
+++ dungeons2_b50.js
@@ -166,11 +166,11 @@ var LaunchDungeons=function()
 		this.quotes={};
 		Game.Monsters[this.name]=this;
 	}
-	var basicLoot={cookies:{min:(1*Game.prestige),max:(5*Game.prestige),prob:0.5}};
-	var goodLoot={cookies:{min:(3*Game.prestige),max:(8*Game.prestige),prob:1},gear:{prob:0.05}};
+	var basicLoot={cookies:{min:(1*(Game.prestige+1)),max:(5*(Game.prestige+1)),prob:0.5}};
+	var goodLoot={cookies:{min:(3*(Game.prestige+1)),max:(8*(Game.prestige+1)),prob:1},gear:{prob:0.05}};
 	var bossLoot={gear:{prob:1}};
-	var chestLoot={cookies:{min:(2*Game.prestige),max:(20*Game.prestige),prob:1},gear:{prob:0.1}};
-	var bossLoot={cookies:{min:(10*Game.prestige),max:(50*Game.prestige),prob:1},gear:{prob:0.2}};
+	var chestLoot={cookies:{min:(2*(Game.prestige+1)),max:(20*(Game.prestige+1)),prob:1},gear:{prob:0.1}};
+	var bossLoot={cookies:{min:(10*(Game.prestige+1)),max:(50*(Game.prestige+1)),prob:1},gear:{prob:0.2}};
 	new Game.Achievement('Antic','Find and defeat <b>Ancient Elder Doughling</b>, the one of very special doughlings for the first time!',[12,7]);Game.last.pool='dungeon';
 	new Game.Achievement('Tier up = Difficulty^2','Defeat <b>Sentient furnace II</b> or other boss which has <b>II</b> at end of its name. <br>Note:These guys are much harder to defeat and can be only found after level 100.',[13,7]);Game.last.pool='dungeon';
 	//general monsters
@@ -189,14 +189,14 @@ var LaunchDungeons=function()
 	new Game.Monster('Baby sentient cookie','babySentientCookie',[4,0],1,{hp:3,might:1,guard:1,speed:7,dodge:7,rarity:1},basicLoot);
 	new Game.Monster('Burnt sentient cookie','burntSentientCookie',[6,0],5,{hp:16,might:12,guard:2,speed:3,dodge:2,rarity:0.2},basicLoot);
 	new Game.Monster('Raw sentient cookie','rawSentientCookie',[5,0],5,{hp:16,might:6,guard:4,speed:7,dodge:7,rarity:0.2},basicLoot);
-	new Game.Monster('Sugar bunny','sugarBunny',[8,0],5,{hp:10,might:3,guard:8,speed:12,dodge:9,rarity:0.001},{cookies:{min:(1000*Game.prestige),max:(10000*Game.prestige)}});
+	new Game.Monster('Sugar bunny','sugarBunny',[8,0],5,{hp:10,might:3,guard:8,speed:12,dodge:9,rarity:0.001},{cookies:{min:(1000*(Game.prestige+1)),max:(10000*(Game.prestige+1))}});
 	Game.Monsters['Sugar bunny'].onKill=function(){Game.Win('Follow the white rabbit');};Game.Monsters['Sugar bunny'].AI='flee';
 	
 	//factory monsters
 	new Game.Monster('Crazed kneader','crazedKneader',[0,2],6,{hp:18,might:6,guard:8,speed:3,dodge:2,rarity:0.5},goodLoot);
 	new Game.Monster('Crazed chip-spurter','crazedDoughSpurter',[0,2],6,{hp:15,might:6,guard:8,speed:5,dodge:3,rarity:0.5},goodLoot);
 	new Game.Monster('Alarm bot','alarmTurret',[3,2],2,{hp:6,might:3,guard:5,speed:8,dodge:8,rarity:0.5},basicLoot);
-	new Game.Monster('Chirpy','chirpy',[4,2],3,{hp:7,might:4,guard:6,speed:9,dodge:9,rarity:0.01},{cookies:{min:500*Game.prestige,max:5000*Game.prestige}});
+	new Game.Monster('Chirpy','chirpy',[4,2],3,{hp:7,might:4,guard:6,speed:9,dodge:9,rarity:0.01},{cookies:{min:500*(Game.prestige+1),max:5000*(Game.prestige+1)}});
 	Game.Monsters['Chirpy'].onKill=function(){Game.Win('Chirped out');};Game.Monsters['Chirpy'].quotes={fight:'oh, hello <3'};
 	new Game.Monster('Disgruntled worker','disgruntledWorker',[1,2],4,{hp:14,might:5,guard:5,speed:6,dodge:4,rarity:0.6},basicLoot);
 	new Game.Monster('Disgruntled overseer','disgruntledOverseer',[1,2],7,{hp:22,might:7,guard:5,speed:6,dodge:4,rarity:0.5},basicLoot);
@@ -800,7 +800,7 @@ var LaunchDungeons=function()
 					{
 						if (Math.random()<0.6)
 						{
-							var value=(Math.round(Math.pow(Math.random(),6)*(10+this.level))*Game.prestige);
+							var value=(Math.round(Math.pow(Math.random(),6)*(10+this.level))*(Game.prestige+1));
 							if (value>0)
 							{
 								var entity=this.AddEntity('item','cookies',tile[0],tile[1]);//random cookies
