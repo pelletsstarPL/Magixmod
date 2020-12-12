79090b414627b96fc7e6c8b98d71a9b692202c89
pelletsstarPL <54317957+pelletsstarPL@users.noreply.github.com>
2020-12-12 15:36:27 +0100

    Update and rename dungeon2Gen_b8.js to dungeon2Gen_b9.js

--- dungeon2Gen_b9.js
+++ dungeon2Gen_b9.js
@@ -842,7 +842,7 @@ var DungeonGen=function()
 					title=(room.corridor?'corridor':'room')+' '+room.id+' | depth : '+room.gen+' | children : '+room.children.length;
 				}
 				var pic=this.getPic(x,y);
-				str+='<div style="opacity:'+opacity+';width:'+size+'px;height:'+size+'px;position:absolute;left:'+(x*size)+'px;top:'+(y*size)+'px;display:block;padding:0px;margin:0px;background:#'+colors[this.data[x][y][0]]+' url(https://pipe.miroware.io/5db9be8a56a97834b159fd5b/Dungeons2/img/dungeonTiles'+nrT+'.png) '+(-pic[0]*16)+'px '+(-pic[1]*16)+'px;color:#999;" title="'+title+'"></div>';
+				str+='<div style="opacity:'+opacity+';width:'+size+'px;height:'+size+'px;position:absolute;left:'+(x*size)+'px;top:'+(y*size)+'px;display:block;padding:0px;margin:0px;background:#'+colors[this.data[x][y][0]]+' url(https://pipe.miroware.io/5db9be8a56a97834b159fd5b/Dungeons2/img/dungeonTiles.png) '+(-pic[0]*16)+'px '+(-pic[1]*16)+'px;color:#999;" title="'+title+'"></div>';
 			}
 			str+='<br>';
 		}
