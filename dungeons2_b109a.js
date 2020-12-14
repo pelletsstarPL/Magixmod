9c1eb5c2fcb68c9388fd78e450af781e53fee33a
pelletsstarPL <54317957+pelletsstarPL@users.noreply.github.com>
2020-12-14 12:18:15 +0100

    Update and rename dungeons2_b109.js to dungeons2_b109a.js

--- dungeons2_b109a.js
+++ dungeons2_b109a.js
@@ -436,8 +436,8 @@ Dungtheme();
 	var bossLoot={gear:{prob:1}};
 	var chestLoot={cookies:{min:(2*(Game.prestige+1)),max:(20*(Game.prestige+1)),prob:1},gear:{prob:0.1}};
 	var bossLoot={cookies:{min:(10*(Game.prestige+1)),max:(50*(Game.prestige+1)),prob:1},gear:{prob:0.2}};
-	new Cppkies.Achievement.Achievement('Antic','Find and defeat <b>Ancient Elder Doughling</b>, the one of very special doughlings for the first time!',[12,7]);Game.last.pool='dungeon';
-	new Cppkies.Achievement.Achievement('Tier up = Difficulty^2','Defeat <b>Sentient furnace II</b> or other boss which has <b>II</b> at end of its name. <br>Note:These guys are much harder to defeat and can be only found after level 100.',[13,7]);Game.last.pool='dungeon';
+	new Cppkies.Achievement('Antic','Find and defeat <b>Ancient Elder Doughling</b>, the one of very special doughlings for the first time!',[12,7]);Game.last.pool='dungeon';
+	new Cppkies.Achievement('Tier up = Difficulty^2','Defeat <b>Sentient furnace II</b> or other boss which has <b>II</b> at end of its name. <br>Note:These guys are much harder to defeat and can be only found after level 100.',[13,7]);Game.last.pool='dungeon';
 	//general monsters
 	new Game.Monster('Doughling','doughling',[0,0],1,{hp:5,might:2,guard:2,speed:6,dodge:6,rarity:0.7},basicLoot);
 	
